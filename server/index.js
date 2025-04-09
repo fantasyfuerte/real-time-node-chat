221d8c6b3bd2f01bea96c2d048748222d9b4a760
Fuerte <leandrofuerte75@gmail.com>
2025-04-09 17:43:37 -0400

    feat: add socket.io connection handling for user connection and disconnection events

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const port = process.env.PORT ?? 3000;
 const server = createServer(app);
 const io = new Server(server);
 
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+})
+
 app.use(logger("dev"));
 
 app.get("/", (req, res) => {
